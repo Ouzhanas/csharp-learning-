d8efbf20a06b3f2206e06eb954035f075cf69c24
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2022-08-25 00:53:33 +0300

    Create for.cs

---
+++ for.cs
@@ -0,0 +1,15 @@
+using System;
+// this program prints numbers from 0 to 9
+namespace Ouz
+{
+    class Ouz
+    {
+        public static void Main()
+        {
+            int i = 0;
+            for(i=0;i<10;i++){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
