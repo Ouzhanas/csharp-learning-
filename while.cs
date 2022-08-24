4bb4a31580e25bc1ac4f8a385c61e5a84729e32a
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2022-08-25 00:28:38 +0300

    Add files via upload

---
+++ while.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ouz
+{
+    class Ouz
+    {
+        public static class Helo
+        {
+            public static void Main()
+            {
+                int i = 0;
+                while (i < 10)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+        }
+    }
+}
