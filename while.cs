996e9f423d6605199e5496a5e4c9350ab78b8385
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2022-08-25 00:41:27 +0300

    Update while.cs

--- while.cs
+++ while.cs
@@ -3,17 +3,14 @@
 namespace Ouz
 {
     class Ouz
-    {
-        public static class Helo
-        {
+    {      
             public static void Main()
             {
                 int i = 0;
                 while (i < 10)
                 {
                     Console.WriteLine(i);
-                    i++;
-                }
+                    i++;     
             }
         }
     }
