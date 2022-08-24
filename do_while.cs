588bb530bc9d90529b931da3cca882e8f46faaa5
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2022-08-25 00:58:28 +0300

    Create do_while.cs

---
+++ do_while.cs
@@ -0,0 +1,14 @@
+using System;
+// This program does the given job first and then the control.
+namespace Ouz
+{
+    class Ouz{
+        public static void Main(){
+            int i = 1;
+            do{
+                Console.Writeline(i);
+                i++;
+            }while(i<10);
+        }
+    }
+}
