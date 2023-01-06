61b57f1b946f345e0a88b1d637abf7e3bfdcb426
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2023-01-07 00:20:51 +0300

    Add files via upload

---
+++ carpim-tablosu-fonksiyonlarla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Deneme { 
+    
+    class Program
+    {
+        static void Yap(int a, int b)
+        {
+            int i;
+            int j;
+            for( i = 0; i < b; i++)
+            {
+                for( j = 0; j < a; j++)
+                {
+                    Console.Write(i * j);
+                    Console.Write("\t");
+                    }
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+           int a = 10;
+           int b = 10; 
+           Yap(a,b);
+        }
+    }
+
+}
